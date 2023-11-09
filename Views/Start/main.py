6217220315ce3414d6e157f968f575930abf4936
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2023-11-09 23:33:24 +0100

    add gui translations

--- Views/Start/main.py
+++ Views/Start/main.py
@@ -1,3 +1,5 @@
+from PyQt6 import QtGui
+from PyQt6.QtCore import QTranslator
 from PyQt6.QtWidgets import QApplication
 from Controllers.MainController import MainController
 from Controllers.AddFileController import AddFileController
@@ -7,6 +9,19 @@ import sys
 
 class Main:
     def __init__(self):
+        # Window Icon
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../images/app-icon/dex-icon-512x512.png"), QtGui.QIcon.Mode.Normal,
+                       QtGui.QIcon.State.Off)
+
+        app.setWindowIcon(icon)
+
+        # Translations
+        plQtGuiPath = "../../Translations/PL/qtbase_pl.qm"
+
+        if translator.load(plQtGuiPath):
+            app.installTranslator(translator)
+
         # Controllers
         self.mainController = MainController()
         self.addFileController = AddFileController()
@@ -19,6 +34,7 @@ class Main:
         self.mainController.action_Settings.triggered.connect(self.mainController.createWindowSettings)
 
 
+translator = QTranslator()
 app = QApplication(sys.argv)
 window = Main()
 sys.exit(app.exec())
