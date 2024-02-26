3e03fcc7754da23c93c48c0e563b3d9c9a1dcb0b
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2024-02-26 17:45:36 +0100

    change main window

--- Controllers/AnalysisController.py
+++ Controllers/AnalysisController.py
@@ -7,10 +7,3 @@ class AnalysisController(QMainWindow, Ui_MainWindow_Main):
     def __init__(self, main_controller):
         super().__init__()
         self.main = main_controller
-
-        self.main.actionTest_1.triggered.connect(self.test)
-
-    def test(self):
-        cursor = self.main.textEdit_Board.textCursor()
-        cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
-        cursor.insertText("Test-1")
