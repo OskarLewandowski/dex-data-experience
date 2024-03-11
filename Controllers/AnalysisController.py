68e72355365b9571be8d0ec58a41f52330ab24b1
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2024-03-12 00:57:02 +0100

    add functions for test Jarque Bera

--- Controllers/AnalysisController.py
+++ Controllers/AnalysisController.py
@@ -12,6 +12,7 @@ from Views.Analysis.test_shapiro_wilka_window import Ui_MainWindow_Test_Shapiro_
 from Views.Analysis.test_andersona_darlinga_window import Ui_MainWindow_Test_Andersona_Darlinga
 from Views.Analysis.test_kolmogorova_smirnova_window import Ui_MainWindow_Test_Kolomogorova_Smirnova
 from Views.Analysis.test_lillieforsa_window import Ui_MainWindow_Test_Lillieforsa
+from Views.Analysis.test_jarque_bera_window import Ui_MainWindow_Test_Jarque_Bera
 
 
 class AnalysisController(QMainWindow, Ui_MainWindow_Main):
@@ -25,6 +26,7 @@ class AnalysisController(QMainWindow, Ui_MainWindow_Main):
         self.main.action_Test_Andersona_Darlinga.triggered.connect(self.createTestAndersonaDarlingaWindow)
         self.main.action_Test_Kolmogorova_Smirnova.triggered.connect(self.createTestKolmogorovaSmirnovaWindow)
         self.main.action_Test_Lillieforsa.triggered.connect(self.createTestLillieforsaWindow)
+        self.main.action_Test_Jarque_Bera.triggered.connect(self.createTestJarqueBeraWindow)
 
     def splitText(self, text, seperator=" : "):
         if seperator in str(text):
@@ -572,6 +574,85 @@ class AnalysisController(QMainWindow, Ui_MainWindow_Main):
     def writeTestLillieforsaInBoard(self):
         try:
             data = self.window_test_lillieforsa_ui.textEdit_Preview_Board.toHtml()
+            if data:
+                cursor = self.main.textEdit_Board.textCursor()
+                cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
+                cursor.insertText("\n")
+                cursor.insertHtml(data)
+        except Exception as e:
+            pass
+
+    # Test Jarque-Bera
+    def createTestJarqueBeraWindow(self):
+        self.window_test_jarque_bera = QMainWindow()
+        self.window_test_jarque_bera_ui = Ui_MainWindow_Test_Jarque_Bera()
+        self.window_test_jarque_bera_ui.setupUi(self.window_test_jarque_bera)
+
+        dataAll = DataStorageModel.get_all_keys_and_columns()
+
+        self.window_test_jarque_bera_ui.comboBox_Data_Column.addItems(dataAll)
+
+        self.window_test_jarque_bera_ui.pushButton_Reset_Options.clicked.connect(self.resetTestJarqueBera)
+        self.window_test_jarque_bera_ui.pushButton_Add_To_Board.clicked.connect(self.writeTestJarqueBeraInBoard)
+
+        self.window_test_jarque_bera_ui.comboBox_Data_Column.currentIndexChanged.connect(self.writeTestJarqueBera)
+
+        self.window_test_jarque_bera_ui.checkBox_Description_Of_Results.clicked.connect(self.writeTestJarqueBera)
+
+        self.window_test_jarque_bera.show()
+
+    def writeTestJarqueBera(self):
+        try:
+            data = self.window_test_jarque_bera_ui.comboBox_Data_Column.currentText()
+            result = None
+            summary = ""
+
+            if data:
+                result = self.splitText(data)
+                dataType = self.checkColumnType(data)
+                selectedColumn = DataStorageModel.get_data_by_key_and_column(result[0], result[1]) if data else None
+                title = f"<b>Test Jarque Bera - test normalności danych</b><br>"
+
+                description = ("<br><b>Interpretacja wyników:</b><br><br>"
+                               "<br><b>Wartość p:</b> Jest to prawdopodobieństwo, że obserwujemy dane tak ekstremalne jak te, które mamy, zakładając, że hipoteza zerowa jest prawdziwa. W kontekście testu Jarque-Bera, hipoteza zerowa zakłada, że dane mają rozkład normalny."
+                               "<ul>"
+                               "<li>Jeżeli <b>wartość p jest mniejsza</b> od wybranego poziomu istotności (np. 0.05), odrzucamy hipotezę zerową. To sugeruje, że dane nie mają rozkładu normalnego.</li>"
+                               "<li>Jeżeli <b>wartość p jest większa</b> od wybranego poziomu istotności, nie ma podstaw do odrzucenia hipotezy zerowej. To sugeruje, że dane mogą pochodzić z rozkładu normalnego. Pamiętaj jednak, że nie jest to to samo co potwierdzenie, że dane pochodzą z rozkładu normalnego.</li>"
+                               "</ul>")
+
+                self.window_test_jarque_bera_ui.textEdit_Preview_Board.clear()
+
+                if dataType == 0:
+                    statistic, p_value = stats.jarque_bera(selectedColumn)
+
+                    testResult = (f"Zbiór: <b>{result[0]}</b><br>"
+                                  f"Kolumna: <b>{result[1]}</b><br><br>"
+                                  f"Wartość p: <b>{round(p_value, 2)}</b><br>")
+
+                    summary = title + testResult
+
+                    if self.window_test_jarque_bera_ui.checkBox_Description_Of_Results.isChecked():
+                        summary = summary + description
+
+                else:
+                    summary = (f"Nieprawidłowe dane w kolumnie <b>'{result[1]}'</b>, wymagane są dane numeryczne!<br>"
+                               f"Wybierz kolumne zawierające dane ilościowe.")
+
+            self.window_test_jarque_bera_ui.textEdit_Preview_Board.setHtml(summary)
+
+        except Exception as e:
+            print(str(e))
+
+    def resetTestJarqueBera(self):
+        self.window_test_jarque_bera_ui.comboBox_Data_Column.setCurrentIndex(-1)
+        self.window_test_jarque_bera_ui.checkBox_Board_Is_Enabled.setChecked(False)
+        self.window_test_jarque_bera_ui.textEdit_Preview_Board.clear()
+        self.window_test_jarque_bera_ui.textEdit_Preview_Board.setReadOnly(True)
+        self.window_test_jarque_bera_ui.checkBox_Description_Of_Results.setChecked(False)
+
+    def writeTestJarqueBeraInBoard(self):
+        try:
+            data = self.window_test_jarque_bera_ui.textEdit_Preview_Board.toHtml()
             if data:
                 cursor = self.main.textEdit_Board.textCursor()
                 cursor.movePosition(QtGui.QTextCursor.MoveOperation.End)
